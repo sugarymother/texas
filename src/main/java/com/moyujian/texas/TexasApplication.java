45ca09d545728eff38aff1c378df65ff17698fcc
tianzerui <tianzerui@baidu.com>
2023-08-28 14:56:47 +0800

    setup

---
+++ src/main/java/com/moyujian/texas/TexasApplication.java
@@ -0,0 +1,13 @@
+package com.moyujian.texas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TexasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moyujian/texas/TexasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moyujian.texas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TexasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
